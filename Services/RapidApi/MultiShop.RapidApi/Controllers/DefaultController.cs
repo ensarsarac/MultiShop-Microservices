73ddca6f880a4b58c468e0e6488d5363a845da0b
Ensar Sarac <76907308+ensarsarac@users.noreply.github.com>
2024-08-14 17:27:32 +0300

    rapidapi çalışmaları tamamlandı

---
+++ Services/RapidApi/MultiShop.RapidApi/Controllers/DefaultController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApi.Models;
+using Newtonsoft.Json;
+
+namespace MultiShop.RapidApi.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        public async Task<IActionResult> WeatherDetail()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/kocaeli"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a506d0b55amshcfb29f2e4989817p174e30jsn4ef35ad88098" },
+        { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<WeatherViewModel>(body);
+                ViewBag.temp = result.data.temp;
+				
+
+			}
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&language=en&to_symbol=TRY"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a506d0b55amshcfb29f2e4989817p174e30jsn4ef35ad88098" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body2 = await response2.Content.ReadAsStringAsync();
+                var result2 = JsonConvert.DeserializeObject<ExchangeViewModel>(body2);
+                ViewBag.exchange = result2.data.exchange_rate;
+            }
+
+
+
+            return View();
+		}
+        public async Task<IActionResult> Exchange()
+        {
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&language=en&to_symbol=TRY"),
+				Headers =
+	{
+		{ "x-rapidapi-key", "a506d0b55amshcfb29f2e4989817p174e30jsn4ef35ad88098" },
+		{ "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+	},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var result = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                ViewBag.exchange = result.data.exchange_rate;
+			}
+            return View();
+		}
+
+        public async Task<IActionResult> ProductList()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-product-search.p.rapidapi.com/search?q=Logitech%20mouse&country=us&language=en&page=1&limit=30&sort_by=BEST_MATCH&product_condition=ANY&min_rating=ANY"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a506d0b55amshcfb29f2e4989817p174e30jsn4ef35ad88098" },
+        { "x-rapidapi-host", "real-time-product-search.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<ProductViewModel>(body);
+                return View(result);
+            }
+        }
+    }
+}
---
+++ Services/RapidApi/MultiShop.RapidApi/Models/ProductViewModel.cs
@@ -0,0 +1,51 @@
+﻿namespace MultiShop.RapidApi.Models
+{
+    public class ProductViewModel
+    {
+
+            public string status { get; set; }
+            public string request_id { get; set; }
+            public Datum[] data { get; set; }
+        
+
+        public class Datum
+        {
+            public string product_id { get; set; }
+            public string product_title { get; set; }
+            public string product_description { get; set; }
+            public string[] product_photos { get; set; }
+            public Product_Attributes product_attributes { get; set; }
+            public float product_rating { get; set; }
+            public string product_page_url { get; set; }
+            public string product_offers_page_url { get; set; }
+            public string product_specs_page_url { get; set; }
+            public string product_reviews_page_url { get; set; }
+            public string product_page_url_v2 { get; set; }
+            public int product_num_reviews { get; set; }
+            public int product_num_offers { get; set; }
+            public string[] typical_price_range { get; set; }
+            public Offer offer { get; set; }
+        }
+
+        public class Product_Attributes
+        {
+        }
+
+        public class Offer
+        {
+            public string store_name { get; set; }
+            public object store_rating { get; set; }
+            public string offer_page_url { get; set; }
+            public object store_review_count { get; set; }
+            public string store_reviews_page_url { get; set; }
+            public string price { get; set; }
+            public string shipping { get; set; }
+            public bool on_sale { get; set; }
+            public object original_price { get; set; }
+            public string product_condition { get; set; }
+            public string payment_methods { get; set; }
+            public string coupon_discount_percent { get; set; }
+        }
+
+    }
+}
